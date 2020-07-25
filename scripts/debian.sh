8331670f9e5948a31e13c99fe38c60b458a812fa
Stivvo <stivvo01@gmail.com>
2020-07-25 23:49:49 +0200

    create debian package

---
+++ scripts/debian.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+RELEASEDIR="qTsConverter_debian"
+
+# creating package dir
+mkdir "../../$RELEASEDIR"
+cd "../../$RELEASEDIR"
+
+# copying binaries
+mkdir -p usr/bin
+cp ../build/src/qTsConverter usr/bin/qTsConverter
+
+# copying documentation and license
+mkdir -p usr/share/doc/qTsConverter
+cp ../qTsConverter/README.md usr/share/doc/qTsConverter/README.md
+mkdir -p usr/share/licenses/qTsConverter
+cp ../qTsConverter/LICENSE usr/share/licenses/qTsConverter/LICENSE
+
+# desktop and icon
+mkdir -p usr/share/pixmaps/
+cp ../qTsConverter/src/qml/assets/logos/profile.png usr/share/pixmaps/qTsConverter.png
+cp ../qTsConverter/src/qml/assets/logos/vector/isolated-layout.svg usr/share/pixmaps/qTsConverter.svg
+mkdir -p usr/share/applications
+cp ../qTsConverter/scripts/qTsConverter.desktop usr/share/applications/qTsConverter.desktop
+
+# debian stff
+mkdir DEBIAN
+echo "Package: qTsConverter" > DEBIAN/control
+echo "Version: 5.0.0" >> DEBIAN/control
+echo "Section: custom" >> DEBIAN/control
+echo "Priority: optional" >> DEBIAN/control
+echo "Architecture: all" >> DEBIAN/control
+echo "Essential: no" >> DEBIAN/control
+echo "Maintainer: Stivvo" >> DEBIAN/control
+echo "Description: A simple tool to convert qt translation file (ts) to other format (xlsx / csv) and vice versa" >> DEBIAN/control
+
+cd ..
+dpkg-deb --build "$RELEASEDIR"
+mv "$RELEASEDIR.deb" qTsConverter.deb
