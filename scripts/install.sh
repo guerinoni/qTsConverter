ba66fb3516792627010649aa5ce8edb8ee7685e5
Stivvo <stivvo01@gmail.com>
2020-07-25 22:03:07 +0200

    install LICENSE and README.md in dedicated dirs

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 install -Dm755 ../../build/src/qTsConverter /usr/bin/qTsConverter
-install -Dm644 ../LICENSE /usr/share/licenses/qTsConverter
-install -Dm644 ../README.md /usr/share/doc/qTsConverter
+install -Dm644 ../LICENSE /usr/share/licenses/qTsConverter/LICENSE
+install -Dm644 ../README.md /usr/share/doc/qTsConverter/README.md
 install -Dm644 qTsConverter.desktop /usr/share/applications/qTsConverter.desktop
 install -Dm644 ../src/qml/assets/logos/vector/isolated-layout.svg /usr/share/pixmaps/qTsConverter.svg
 install -Dm644 ../src/qml/assets/logos/profile.png /usr/share/pixmaps/qTsConverter.png
