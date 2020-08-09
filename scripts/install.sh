f84fd7e76da4f31ccb29a5205d5d558da08830c9
Stivvo <stivvo01@gmail.com>
2020-08-09 17:27:49 +0200

    make install support, both binary and dependencies
    compile.sh installs on /usr/local which is the directory where programs
    installed outside of the package manager should be
    
    /usr/local/bin contains the qTsConverter executable,
    
    /usr/local/include .h and .cpp files necessary for development (from the
    3rd-parties)
    
    /usr/local/lib static and (more importantly) shared libraries (like
    libQtXlsxWriter.so)
    
    As of now, running make install has similar effects as installing all
    the 3rd-parties library individually

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-install -Dm755 ../../build/src/qTsConverter /usr/bin/qTsConverter
 install -Dm644 ../LICENSE /usr/share/licenses/qTsConverter/LICENSE
 install -Dm644 ../README.md /usr/share/doc/qTsConverter/README.md
 install -Dm644 qTsConverter.desktop /usr/share/applications/qTsConverter.desktop
