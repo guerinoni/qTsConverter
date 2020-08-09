0bfa30f5c617f2513b498b401e118bd185d77d7d
Stivvo <stivvo01@gmail.com>
2020-08-09 17:34:32 +0200

    install instructions, uninstall.sh cleans all
    now, uninstall.sh also gets rid of dependencies installed with make
    install
    
    If LD_LIBRARY_PATH isn't set correctly, the install prefix should be
    /usr/bin (or slightly better, the binary in /usr/bin and libraries in
    /usr/lib) but that thoose arn't good places to put custom build programs

--- README.md
+++ README.md
@@ -19,5 +19,24 @@ editable using an office suite.
 - choose output filename
 - click convert button
 
-### To generate output.csv  
-![example conversion ts -> csv](./doc/Screenshot.png)  
+### To generate output.csv
+![example conversion ts -> csv](./doc/Screenshot.png)
+
+## Build
+
+Compile from source and install:
+
+```
+$ git clone https://github.com/guerinoni/qTsConverter.git
+$ cd scripts/
+$ ./compile.sh
+$ sudo ./install.sh
+$ cd ../../build
+$ sudo make install
+```
+
+If qTsConverter was installed with ``-DCMAKE_INSTALL_PREFIX=/usr/local`` (like
+``compile.sh`` does) the shell variable ``LD_LIBRARY_PATH`` should contain
+``/usr/local/lib`` and ``/usr/local/bin`` should be in the ``PATH`` variable.
+This allows to locate libraries that qTsConverter depends on and run it from any
+path.
--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -1,6 +1,27 @@
 #!/bin/sh
 
 rm /usr/local/bin/qTsConverter
+rm -r /usr/local/include/qtcsv
+rm /usr/local/include/vxlsxabstractooxmlfile.h
+rm /usr/local/include/vxlsxabstractsheet.h
+rm /usr/local/include/vxlsxcellformula.h
+rm /usr/local/include/vxlsxcell.h
+rm /usr/local/include/vxlsxcellrange.h
+rm /usr/local/include/vxlsxcellreference.h
+rm /usr/local/include/vxlsxchart.h
+rm /usr/local/include/vxlsxchartsheet.h
+rm /usr/local/include/vxlsxconditionalformatting.h
+rm /usr/local/include/vxlsxdatavalidation.h
+rm /usr/local/include/vxlsxdocument.h
+rm /usr/local/include/vxlsxformat.h
+rm /usr/local/include/vxlsxglobal.h
+rm /usr/local/include/vxlsxrichstring.h
+rm /usr/local/include/vxlsxworkbook.h
+rm /usr/local/include/vxlsxworksheet.h
+rm -r /usr/local/cmake/QtXlsxWriter
+rm -r /usr/local/libqtcsv.a
+rm -r /usr/local/libQtXlsxWriter.so
+
 rm -r /usr/share/licenses/qTsConverter
 rm -r /usr/share/doc/qTsConverter
 rm /usr/share/applications/qTsConverter.desktop
