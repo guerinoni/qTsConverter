9c2b6bb1f1aace46d2c3e6a2871aa5d7e93ccc03
Stivvo <stivvo01@gmail.com>
2020-08-10 14:31:22 +0200

    data subdirectory
    Isolate the installation of non source files in the data subdirectory
    
    Installation of the main executable happens in the src subdirectory

--- scripts/makeFlatpak.sh
+++ scripts/makeFlatpak.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd ../../
-exec flatpak-builder -v -y --force-clean --install-deps-from=flathub --user --install flatpak_build qTsConverter/scripts/org.guerinoni.qTsConverter.yml
+exec flatpak-builder -v -y --force-clean --install-deps-from=flathub --user --install flatpak_build qTsConverter/data/org.guerinoni.qTsConverter.yml
