f1f88806519a691dfc772a4274e4321e5e89e06e
Stivvo <stivvo01@gmail.com>
2020-08-11 15:52:33 +0200

    Enhancment debian.sh
    Initial cleanup

--- scripts/debian.sh
+++ scripts/debian.sh
@@ -5,6 +5,9 @@
 echo "version tag: $1"
 RELEASEDIR="qTsConverter_debian_$1"
 
+# cleanup
+rm -r ../../$RELEASEDIR ../../$RELEASEDIR.deb
+
 # creating package dir
 mkdir "../../$RELEASEDIR"
 cd "../../$RELEASEDIR"
@@ -32,14 +35,14 @@ cp ../build/3rd-party/qtxlsx/libQtXlsxWriter.so usr/lib/libQtXlsxWriter.so
 
 # debian stff
 mkdir DEBIAN
-echo "Package: qTsConverter" > DEBIAN/control
-echo "Version: $$1" >> DEBIAN/control
-echo "Section: custom" >> DEBIAN/control
-echo "Priority: optional" >> DEBIAN/control
-echo "Architecture: all" >> DEBIAN/control
-echo "Essential: no" >> DEBIAN/control
-echo "Maintainer: Stivvo" >> DEBIAN/control
-echo "Description: A simple tool to convert qt translation file (ts) to other format (xlsx / csv) and vice versa" >> DEBIAN/control
-
-cd ..
+cd DEBIAN
+echo "Package: qTsConverter" > control
+echo "Version: $1" >> control
+echo "Section: custom" >> control
+echo "Priority: optional" >> control
+echo "Architecture: x86-64" >> control
+echo "Essential: no" >> control
+echo "Maintainer: Stivvo" >> control
+echo "Description: A simple tool to convert qt translation file (ts) to other format (xlsx / csv) and vice versa" >> control
+cd ../..
 dpkg-deb --build "$RELEASEDIR"
