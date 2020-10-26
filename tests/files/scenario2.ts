edcd5279c708e293fd7ed735c37ec1f941684b57
Leonard <montagna@micro-systems.it>
2020-10-26 16:20:53 +0100

    Add scenario 2
    Signed-off-by: Leonard <montagna@micro-systems.it>

---
+++ tests/files/scenario2.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>ThemeWidget</name>
+    <message>
+        <location filename="../themewidget.cpp" line="289"/>
+        <location filename="../themewidget.cpp" line="290"/>
+        <location filename="../themewidget.cpp" line="291"/>
+        <source>Series </source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ThemeWidgetForm</name>
+    <message>
+        <location filename="../themewidget.ui" line="19"/>
+        <source>Theme:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../themewidget.ui" line="29"/>
+        <source>Animation:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../themewidget.ui" line="39"/>
+        <source>Legend:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../themewidget.ui" line="49"/>
+        <source>Anti-aliasing</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
--- tests/tst_ts2csv.cpp
+++ tests/tst_ts2csv.cpp
@@ -1,6 +1,7 @@
 #include "ConverterFactory.hpp"
 
 #include <QFile>
+#include <QtDebug>
 
 const std::string m_outputFile{ FILESPATH + std::string("/output.csv") };
 
@@ -33,10 +34,36 @@ bool scenario1()
 
     return o.size() == e.size() && o == e;
 }
+bool scenario2()
+{
+    const auto inputFile{ FILESPATH + std::string("/scenario2.ts") };
+    auto conv = ConverterFactory::make_converter(
+        ConverterFactory::ConversionType::Ts2Csv, inputFile.c_str(),
+        m_outputFile.c_str(), ";", "\"", "2.1");
+    conv->process();
+    QFile output(m_outputFile.c_str());
+    if (!output.exists()) {
+        return false;
+    }
+
+    QFile expected(FILESPATH + QString("/scenario2.csv"));
+    expected.open(QIODevice::ReadOnly | QIODevice::Text);
+    output.open(QIODevice::ReadOnly | QIODevice::Text);
+
+    const auto o = output.readAll();
+    const auto e = expected.readAll();
+
+    expected.close();
+    output.close();
+
+    qDebug() << "e size: " << e.size() << ", e:\n" << e;
+    qDebug() << "o size: " << o.size() << ", o:\n" << o;
+    return o.size() == e.size() && o == e;
+}
 
 int main()
 {
-    int ret = !scenario1();
-    cleanup();
+    int ret = !scenario1() || !scenario2();
+    //    cleanup();
     return ret;
 }
